8114c458edefbc8ec8f46b5894397e5ec33ff342
wroclawianka <dorota.zelga@gmail.com>
2018-07-20 20:24:51 +0200

    first draft of the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import { BookList } from "./components/BooksList/BookList";
+import { BookDetail } from "./components/BookDetail/BookDetail";
+import { AddBook } from "./components/AddBook/AddBook";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <p>Hello!</p>
+        <div className="jumbotron text-center">
+          <h1>Bookshelf</h1>
+          <p>Your list of books</p> 
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col col-xs-10 col-xs-offset-1">
+              <BookList/>
+            </div>
+            <div className="col col-xs-10 col-xs-offset-1">
+            <BookDetail/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
