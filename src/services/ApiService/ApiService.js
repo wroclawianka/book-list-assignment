64eed8712a16cdc947a779f54072ba1bedc144d5
wroclawianka <dorota.zelga@gmail.com>
2018-07-21 15:31:51 +0200

    ApiService improvements

--- src/services/ApiService/ApiService.js
+++ src/services/ApiService/ApiService.js
@@ -1,8 +1,14 @@
 export class ApiService {
     apiUrl = "http://localhost:3001/api/v1"
+    headers = {
+        "Content-Type": "application/json"
+    }
 
     fetchBooksList() {
-        let init = {};
+        let init = {
+            method: "GET",
+            headers: this.headers
+        };
         return fetch(`${this.apiUrl}/items`, init)
         .then(response => response.json());
     }
