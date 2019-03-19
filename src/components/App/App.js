922e26e35a823cb1a4cc1d1d906740f0e5738719
wroclawianka <dorota.zelga@gmail.com>
2019-03-19 21:58:27 +0100

    fix for add book > add book id

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
 
   onAddBook(book){
     let bookList = this.state.books
+    book.id = this.state.books.length;
     bookList.push(book);
     this.setState({
       books: bookList
